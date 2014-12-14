6d1534097267c61d8b8d91ca097a1a5f26cff07d
Michael Beckwith <tw2113@gmail.com>
2014-12-13 21:07:12 -0600

    add our new widget

--- includes/widgets.php
+++ includes/widgets.php
@@ -3,8 +3,10 @@
  //register our widget
 function badgeos_toolkit_register_widgets() {
 	require_once( plugin_dir_path( dirname( __FILE__ ) ) . 'includes/widgets/earned-user-achievements-widget.php' );
+	require_once( plugin_dir_path( dirname( __FILE__ ) ) . 'includes/widgets/achievement-earners-widget.php' );
 
 	register_widget( 'toolkit_earned_user_achievements_grid_widget' );
+	register_widget( 'toolkit_achievement_earners_widget' );
 
 }
 add_action( 'widgets_init', 'badgeos_toolkit_register_widgets', 10 );
