ca71cfa1aaa03ca312523fabe8a819de25c58137
Michael Beckwith <tw2113@gmail.com>
2014-12-13 20:29:20 -0600

    rename to indicate grid base

--- includes/widgets.php
+++ includes/widgets.php
@@ -4,7 +4,7 @@
 function badgeos_toolkit_register_widgets() {
 	require_once( plugin_dir_path( dirname( __FILE__ ) ) . 'includes/widgets/earned-user-achievements-widget.php' );
 
-	register_widget( 'toolkit_earned_user_achievements_widget' );
+	register_widget( 'toolkit_earned_user_achievements_grid_widget' );
 
 }
 add_action( 'widgets_init', 'badgeos_toolkit_register_widgets', 10 );
